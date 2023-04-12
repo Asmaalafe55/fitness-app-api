b4cf85f5021976ed69e7163963b4a947f25679be
Mario Saliba <mario.saliba98@gmail.com>
2023-04-12 17:43:15 +0300

    init

---
+++ src/utils/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+    constructor(statusCode, message, stack = '', info) {
+        super(message)
+
+        this.statusCode = statusCode
+        this.info = info
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+
+    statusCode
+    info
+}
+
+export default ApiError
