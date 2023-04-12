5df46f5593cb75c580228895fd70d359d5ee70c9
Mario Saliba <mario.saliba98@gmail.com>
2023-04-12 23:08:58 +0300

    updates the port to 4000

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const envVars = process.env
 
-export const port = process.env.PORT || 3000
+export const port = process.env.PORT || 4000
 
 export const environment = envVars.NODE_ENV || 'development'
 export const isDev = environment === 'development'
